e295cc5308b16eb60d5d2acf949df4bb0b07fec0
Nibbler <kwimbish@hotmail.com>
2022-03-16 19:53:25 -0500

    continue fetch URL

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,8 +3,8 @@ var apiUrl = 'https://opentdb.com/api.php?amount='+ questions;
 
 //'https://opentdb.com/api.php?amount=10&category=23&difficulty=easy&type=multiple'
 
-fetch (apiUrl) .then (function(response) {
-    
+fetch (apiUrl) .then(function(response) {
+  console.log(response);  
 }
 
 )
