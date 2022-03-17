0a764770348c51e998389efba4541f244e6e878a
CodyWBrice <codywbrice7@gmail.com>
2022-03-16 19:45:22 -0500

    developing API fetch

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,11 @@
 const questions = 10;
 var apiUrl = 'https://opentdb.com/api.php?amount='+ questions;
 
-'https://opentdb.com/api.php?amount=10&category=23&difficulty=easy&type=multiple'
+//'https://opentdb.com/api.php?amount=10&category=23&difficulty=easy&type=multiple'
 
+fetch (apiUrl) .then (function(response) {
+    
+}
 
+)
 
