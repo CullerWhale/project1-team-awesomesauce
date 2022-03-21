d1628a2a7d21e8ffa51c5fb77ae62f7ad950771b
Nibbler <kwimbish@hotmail.com>
2022-03-21 13:19:16 -0500

    game/highscore/end css updates, partial js updates

---
+++ assets/js/script.js
@@ -0,0 +1,60 @@
+const question = document.getElementById("question");
+const choices = Array.from(document.getElementsByClassName("choice-text"));
+const questionCounterText = document.getElementById('questionCounter');
+const scoreText = document.getElementById('score');
+
+let currentQuestion = {};
+let acceptingAnswers = false;
+let score = 0;
+let questionCounter = 0;
+let availableQuestions = [];
+
+let questions = [];
+
+//constants
+const CORRECT_BONUS = 10;
+const MAX_QUESTIONS = 20;
+
+startGame = () => {
+  questionCounter = 0;
+  score = 0;
+ // rest of function  
+  getNewQuestion();
+};
+
+getNewQuestion = () => {
+
+  if(availableQuestions.length === 0 || questionCounter >= MAX_QUESTIONS){
+    localStorage.setItem("mostRecentScore", score);
+    // go to end of page
+    //return window.location.assign('./end.html');
+  }
+  questionCounter++;
+  questionCounterText.innerHTML = `${questionCounter}/${MAX_QUESTIONS}`;
+// rest of function
+  acceptingAnswers = true;
+};
+
+choices.forEach(choice => {
+  choice.addEventListener('click', e => {
+    if(!acceptingAnswers) return;
+
+    acceptingAnswers = false;
+    const selectedChoice = e.target;
+    const selectedAnswer = selectedChoice.dataset["number"];
+
+    // finish function, include if below
+    if (classToApply === "correct") {
+      incrementScore(CORRECT_BONUS);
+    }
+
+    getNewQuestion();
+  });
+});
+
+incrementScore = num => {
+  score += num;
+  scoreText.innerHTML = score;
+}
+
+startGame();
