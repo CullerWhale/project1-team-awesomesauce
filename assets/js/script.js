e19cc4823d57c52957061f16b116d7bd66b17e13
Patrick Quirk <patrickliamquirk@yahoo.com>
2022-03-24 13:03:09 -0500

    added explanatory comments; removed some old comments

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,14 @@
+// create the url that will be used to call Open Trivia DB
+// currently the number of questions is hardcoded to be 10, but this would be changed if we decide to let the user choose parameters.
 const questionsAPI = 10;
 var apiUrl = 'https://opentdb.com/api.php?amount='+ questionsAPI;
 
-'https://opentdb.com/api.php?amount=10&category=23&difficulty=easy&type=multiple'
+// 'https://opentdb.com/api.php?amount=10&category=23&difficulty=easy&type=multiple'
 
 
 var ourQuestions = [];
 
-//Connect to empty question div and answer div
+//Connect to empty divs for the question prompt, answer choices, and the gifs.
 var questions = document.querySelector("#question");
 var possibleAnswers = document.querySelector("#answers");
 var gifContainerEl = document.querySelector("#gif-page");
@@ -20,8 +22,13 @@ var gifContainerEl = document.querySelector("#gif-page");
 
 // var currentQuestion = {};
 // var acceptingAnswers = false;
+
+// create variable for the running score total
 var score = 0;
+// create variable to track which question we are on
+// this corresponds to the index in the ourQuestions array.
 var questionCounter = 0;
+
 // var availableQuestions = [];
 
 // remove the imageSRC property from this when we add code to call GIPHY API
@@ -44,23 +51,32 @@ var questionCounter = 0;
 
 // ];
 
-
+// function to handle clicks on the game page (called from event listener at bottom of file)
 mainClickHandler = function(event) {
+    // prevent the click from refreshing the page
     event.preventDefault();
 
+    // get the target element of the click. This will be used to figure out what needs to be done
     targetEl = event.target;
 
+    // if the clicked element is an answer choice button, get the text from the button and check the answer
+    // after checking the answer, display the gif page (currently done from the checkAnswer function)
     if (targetEl.matches(".choice-text")){
         var answerText = targetEl.textContent;
         checkAnswer(answerText);
     }
+    // otherwise, check if the clicked element is the button to go to the next question
     else if (targetEl.matches(".next-question")) {
+        // increase the question counter by 1 (since we need to go to the next question)
         questionCounter++;
+        // remove the content from the gifContainer, since we no longer want to display the gif
         gifContainerEl.innerHTML = "";
+        // if we still have questions left, then render the next question and its answers
         if (questionCounter < ourQuestions.length) {
             renderQuestion();
             renderPossibleAnswers();
         }
+        // if we ran out of questions, then we need to end the quiz
         else {
             // change this part to calling a function to end the quiz
             console.log("Ending quiz.");
@@ -74,8 +90,9 @@ mainClickHandler = function(event) {
 //insert our questions 
 function renderQuestion () {
     var currentQuestion = document.createElement('div');
+    // make the text content of the new div be the question prompt for the current question
     currentQuestion.textContent = ourQuestions[questionCounter].question;
-    questions=document.querySelector('#question');
+    // questions=document.querySelector('#question');
     questions.appendChild(currentQuestion);
     //for loop?  
 
@@ -97,7 +114,7 @@ function combineAnswers() {
 // combineAnswers();
 
 
-
+// check if the answer is correct, and go to the gif page
 function checkAnswer(answerText) {
     if (answerText == ourQuestions[questionCounter].correct_answer) {
         console.log('question is correct');
@@ -105,12 +122,16 @@ function checkAnswer(answerText) {
         console.log('question is incorrect');
     };
 
+    // remove the html for the question prompt and answer choices
     questions.innerHTML = "";
     answerList.innerHTML = "";
+    // display the gif in the gifContainer
     var gifEl = document.createElement('img');
+    // when we are ready to call the GIPHY API, this will need to be changed
     gifEl.src = ourQuestions[questionCounter].imageSRC;
     gifContainerEl.appendChild(gifEl);
 
+    // create the button to go to the next question
     var nextQuestionBtn = document.createElement('button');
     nextQuestionBtn.className = "next-question"
     nextQuestionBtn.textContent = 'Next Question';
@@ -133,8 +154,9 @@ function renderPossibleAnswers() {
 // renderPossibleAnswers();
 
 
-
+// start the quiz
 startQuiz = function() {
+    // just in case, reset the values of the question counter and the score
     questionCounter = 0;
     score = 0;
     // rest of function  
@@ -148,7 +170,9 @@ startQuiz = function() {
 
                 ourQuestions = data.results;
                 console.log(ourQuestions[0].question);
+                // add the correct answer to the list of incorrect answers for easier display
                 combineAnswers();
+                // render the first question and its possible answers
                 renderQuestion();
                 renderPossibleAnswers();
 
@@ -168,8 +192,10 @@ startQuiz = function() {
       
 };
 
+// start the quiz when this file is loaded (note that this file is not connected to index.html)
 startQuiz();
 
+// event listener for all clicks on the page
 document.addEventListener("click", mainClickHandler);
 
 
