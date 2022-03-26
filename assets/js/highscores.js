732923e19bff40ea27855828ccd3f25eb370bb62
Patrick Quirk <patrickliamquirk@yahoo.com>
2022-03-26 14:54:58 -0500

    display high scores

--- assets/js/highscores.js
+++ assets/js/highscores.js
@@ -1,9 +1,17 @@
-const highScoresList = document.getElementById('highScoresList');
+// const highScoresList = document.getElementById('highScoresList');
 const highScores = JSON.parse(localStorage.getItem('highScores')) || [];
 
-highScoresList.innerHTML = highScores
-  highScores
-    .map(score => {
-      return `<li class="high-score">${score.name} - ${score.score}</li>`;
-    })
-    .join("");
+// highScoresList.innerHTML = highScores
+//   highScores
+//     .map(score => {
+//       return `<li class="high-score">${score.name} - ${score.score}</li>`;
+//     })
+//     .join("");
+
+for (i = 0; i < highScores.length; i++) {
+  var score = highScores[i];
+  var scoreLiEl = $("<li>")
+    .addClass("high-score")
+    .text(score.name + " - " + score.score);
+  $("#highScoresList").append(scoreLiEl);
+}
