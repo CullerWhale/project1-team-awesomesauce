a6509208cd73ad1ab81006178e11d84efaa91175
Patrick Quirk <patrickliamquirk@yahoo.com>
2022-03-27 11:09:20 -0500

    load dropdown

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,11 @@
+M.AutoInit();
+
 document.addEventListener('DOMContentLoaded', function() {
     var elems = document.querySelectorAll('.dropdown-trigger');
     var instances = M.Dropdown.init(elems, options);
 });
 
-var instances = M.Dropdown.getInstance(elem);
-instances.open();
+var instance = M.Dropdown.getInstance(elem);
+instance.open();
+
+// document.addEventListener('click', clickHandler);
