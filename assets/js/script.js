60ffc2dfab80a24a8679be2d8e17aedb84ec6125
CullerWhale <jcullenharris@gmail.com>
2022-03-27 18:07:19 -0500

    dropdown success now need to display categories and fill list elements

--- assets/js/script.js
+++ assets/js/script.js
@@ -12,3 +12,12 @@ listEl.addEventListener("click", function(event) {
 
     localStorage.setItem('categoryNumber', JSON.stringify(categorySelection));
 });
+
+var numberOfCategories = 9
+var categoryNames = ['all of the names']; 
+
+for (let index = 0 ; index < 24; index++) {
+    const element = document.createElement('li');
+    
+    numberOfCategories++;
+}
