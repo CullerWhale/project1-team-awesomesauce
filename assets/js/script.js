e4ad73155e8f13b6a1e46a746ed1640325f0d4ed
Patrick Quirk <patrickliamquirk@yahoo.com>
2022-03-27 18:34:15 -0500

    correct drop down items

--- assets/js/script.js
+++ assets/js/script.js
@@ -14,12 +14,17 @@ listEl.addEventListener("click", function(event) {
     localStorage.setItem('categoryNumber', JSON.stringify(categorySelection));
 });
 
-var numberOfCategories = 9
-var categoryNames = ['all of the names']; 
+var numberOfCategory = 9
+var categoryNames = ["General Knowledge", "Entertainment: Books", "Entertainment: Film", "Entertainment: Music", "Entertainment: Musicals and Theaters",
+"Entertainment: Video Games", "Entertainment: Board Games", "Science and Nature", "Science: Computers", "Science: Mathematics", "Mythology", "Sports",
+"Geography", "History", "Politics", "Art", "Celebrities", "Animals", "Vehicles", "Entertainment: Comics", "Science: Gadgets", "Entertainment: Japanese Anime and Manga",
+"Entertainment: Cartoons and Animations"]; 
 
 for (let index = 0 ; index < 24; index++) {
-    const element = document.createElement('li');
-    
-    numberOfCategories++;
+    const dropdownItemEl = document.createElement('li');
+    dropdownItemEl.setAttribute("value", numberOfCategory);
+    dropdownItemEl.textContent = categoryNames[index];
+    listEl.appendChild(dropdownItemEl);
+    numberOfCategory++;
 }
 
