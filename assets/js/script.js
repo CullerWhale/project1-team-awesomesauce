de4b63b8d1afe280f64ca15e923800b6fb4c9998
Patrick Quirk <patrickliamquirk@yahoo.com>
2022-03-27 11:46:36 -0500

    pass options into initialization

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,11 @@
 M.AutoInit();
 
 document.addEventListener('DOMContentLoaded', function() {
+    var options = {
+        onCloseStart: function() {
+            console.log(1);
+        }
+    }
     var elems = document.querySelectorAll('.dropdown-trigger');
     var instances = M.Dropdown.init(elems, options);
 });
